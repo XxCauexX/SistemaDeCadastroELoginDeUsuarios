f92c3e413a009a90e1e7d6d51a101292bd831009
Cauê de Andrade <caue.andrade.505@gmail.com>
2023-06-27 15:10:00 -0300

    Update TelaLoginOuCadastro.java

--- Sistema de Cadastro e Login de Usuários/src/gui/TelaLoginOuCadastro.java
+++ Sistema de Cadastro e Login de Usuários/src/gui/TelaLoginOuCadastro.java
@@ -24,6 +24,7 @@ public class TelaLoginOuCadastro extends javax.swing.JFrame {
     
     public String nomeUsuario;
     
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
